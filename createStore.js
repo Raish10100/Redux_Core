de0f0aac9fe495f5774910c558df5d1e3066ddd2
Raish10100 <raishgma101001@gmail.com>
2024-05-28 14:44:05 +0530

    combine reducer of redux

--- createStore.js
+++ createStore.js
@@ -1,8 +1,9 @@
-import { bindActionCreators, createStore } from 'redux';
+import { bindActionCreators, createStore, combineReducers } from 'redux';
 
 const ADD_TODO = 'add_todo';
 const DEL_TODO = 'delete_todo';
 const UPD_TODO = 'edit_todo';
+const ADD_USER = 'add_user';
 
 function todoReducer(state = [], action) {
     if (action.type === ADD_TODO) {
@@ -28,7 +29,19 @@ function todoReducer(state = [], action) {
     }
 }
 
-const { dispatch, subscribe, getState, replaceReducer } = createStore(todoReducer, []);
+function userReducer(state = [], action) {
+    if(action.type == ADD_USER) {
+        const userName = action.payload.userName
+        return [
+            ...state,
+            {name: userName, id: (state.length == 0) ? 1 : state[state.length - 1].id + 1}
+        ]
+    }
+    return state;
+}
+
+const reducers = combineReducers({ todo: todoReducer, user: userReducer})
+const { dispatch, subscribe, getState, replaceReducer } = createStore(reducers);
 
 dispatch({ type: ADD_TODO, payload: { todoText: 'todo 1' } });
 dispatch({ type: ADD_TODO, payload: { todoText: 'todo 2' } });
@@ -43,6 +56,7 @@ dispatch({ type: DEL_TODO, payload: { todoId: 1 } });
 // converted action object -> action methods (action creator)
 const addTodo = (todoText) => ({type: ADD_TODO, payload: {todoText}});
 const  delTodo = (todoId) => ({type: DEL_TODO, payload: { todoId}});
+const addUser = (name) => ({type: ADD_USER, payload: {userName: name}})
 
 dispatch(addTodo('todo 3'));
 // console.log(getState()) ;  // Expected output: [{ text: 'todo 2', isFinished: false, id: 2 }, { text: 'todo 3', isFinished: false, id: 3 }]
@@ -52,8 +66,9 @@ dispatch(delTodo(2));
 
 
 // using bindActionCreator 
-const actions = bindActionCreators({addTodo, delTodo}, dispatch);
+const actions = bindActionCreators({addTodo, delTodo, addUser}, dispatch);
 actions.addTodo('todo 4');
+actions.addUser('user 1')
 actions.delTodo(2);
 
 console.log(getState())//  Expected output:  [{ text: 'todo 3', isFinished: false, id: 3 }, { text: 'todo 4', isFinished: false, id: 4 }]
